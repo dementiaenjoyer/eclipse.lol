08e67e6d76673183ec55bb3d8eb29032d62f7496
dementiaenjoyer <81053531+dementiaenjoyer@users.noreply.github.com>
2024-03-11 14:45:07 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,4 @@
 local Games = {
-    {id = 2788229376, load = nil},
     {id = nil, load = "https://raw.githubusercontent.com/dementiaenjoyer/eclipse.lol/main/universal.lua"},
 }
 
@@ -7,10 +6,14 @@ function Load(array)
     loadstring(game:HttpGet(array.load))()
 end
 
-for i, CheckTable in ipairs(Games) do
-    if CheckTable.id == game.GameId and CheckTable.load ~= nil then
-        Load(CheckTable)
-    elseif CheckTable.id == nil and CheckTable.load ~= nil then
-        Load(CheckTable)
+if not (getgenv().Load_DaHood) then
+    for i, CheckTable in ipairs(Games) do
+        if CheckTable.id == game.GameId and CheckTable.load ~= nil then
+            Load(CheckTable)
+        elseif CheckTable.id == nil and CheckTable.load ~= nil then
+            Load(CheckTable)
+        end
     end
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/dementiaenjoyer/eclipse.lol/main/da%20hood.lua"))()
 end
