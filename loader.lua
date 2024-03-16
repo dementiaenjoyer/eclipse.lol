56db7f92fbca8b9c0e1b5d2945dd08467b4c0e75
dementiaenjoyer <81053531+dementiaenjoyer@users.noreply.github.com>
2024-03-16 17:18:20 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,17 +1,17 @@
 local Games = {
     {id = nil, load = "https://raw.githubusercontent.com/dementiaenjoyer/eclipse.lol/main/universal.lua"},
+    {id = 292439477, load = "https://raw.githubusercontent.com/dementiaenjoyer/eclipse.lol/main/phantom%20forces.lua"},
 }
 
-function Load(array)
-    loadstring(game:HttpGet(array.load))()
+function Load(data)
+    loadstring(game:HttpGet(data.load))()
 end
 
 if not (getgenv().Load_DaHood) then
     for i, CheckTable in ipairs(Games) do
-        if CheckTable.id == game.GameId and CheckTable.load ~= nil then
+        if CheckTable.id == game.PlaceId and CheckTable.load ~= nil then
             Load(CheckTable)
-        elseif CheckTable.id == nil and CheckTable.load ~= nil then
-            Load(CheckTable)
+            break
         end
     end
 else
