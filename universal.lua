afdd353b6be0129980a273ffcc7f115779a0daeb
dementiaenjoyer <81053531+dementiaenjoyer@users.noreply.github.com>
2024-03-18 00:39:52 +0100

    Update universal.lua

--- universal.lua
+++ universal.lua
@@ -68,9 +68,9 @@ function GetClosestPlayer()
 
     for i, GetPlayers in pairs(game.Players:GetPlayers()) do
         if GetPlayers.Character and GetPlayers.Character:FindFirstChildOfClass("Humanoid") and not (GetPlayers.Name == game.Players.LocalPlayer.Name) then
-            local ScreenPoint = workspace.CurrentCamera:WorldToScreenPoint(GetPlayers.Character[_G.FeatureTable.Combat.CurrentHitbox].Position)
+            local ScreenPoint, OnScreen = workspace.CurrentCamera:WorldToScreenPoint(GetPlayers.Character[_G.FeatureTable.Combat.CurrentHitbox].Position)
             local Dist = (Vector2.new(UserInputService:GetMouseLocation().X, UserInputService:GetMouseLocation().Y) - Vector2.new(ScreenPoint.X, ScreenPoint.Y)).Magnitude
-            if Dist < _G.FeatureTable.Combat.FOVCircleRadius * 2 then
+            if Dist < _G.FeatureTable.Combat.FOVCircleRadius * 2 and OnScreen then
                 ClosestPlayer = GetPlayers
                 if _G.FeatureTable.Combat.Notifications then
                     Library:Notify("New Target: ".. GetPlayers.Name.. ".")
